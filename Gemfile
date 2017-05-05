58ea88f2b6a8e34741d94c0363707e9a0ebbb203
Sathyan Mathai <sathyanmathai@Sathyans-MacBook-Pro.local>
2017-05-05 11:58:45 -0400

    Added flash messages

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'haml'
 gem 'giphy'
 gem 'twilio-ruby'
 gem 'sinatra'
+gem 'sinatra-flash'
+gem 'pry'
 
 group :test do
   gem "rspec"
--- server.rb
+++ server.rb
@@ -1,12 +1,19 @@
 require './app/models/message_service'
 
 class GifMessenger < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+
+  MESSAGE_SUCCESS_MESSAGE = "Message successfully sent".freeze
+
   get '/' do
     haml :form
   end
 
   post '/request' do
-    MessageService.execute(query: params["search"], phone_number: params["phone_number"])
+    result = MessageService.execute(query: params["search"], phone_number: params["phone_number"])
+
+    result.error_message ? flash[:warning] = result.error_message : flash[:info] = MESSAGE_SUCCESS_MESSAGE
     redirect "/"
   end
 end
