e0d680791b71a2fe8a76a980e953e3d70d82ea8e
Sathyan Mathai <sathyanmathai@Sathyans-MacBook-Pro.local>
2017-05-05 19:51:29 -0400

    Renamed a variable for clarity

--- app/models/gif_url_service.rb
+++ app/models/gif_url_service.rb
@@ -5,7 +5,7 @@ class GifUrlService
   def self.find_url(query)
 
     # TODO: Add the ability to go to the next page if none of the images are below the size limit
-    result = Giphy.search(query, limit: 25).find { |gif| gif.original_image.size < SIZE_LIMIT }
-    URI.join(GIPHY_CDN_ROOT_PATH, "#{result.id}.gif")
+    selected_gif = Giphy.search(query, limit: 25).find { |gif| gif.original_image.size < SIZE_LIMIT }
+    URI.join(GIPHY_CDN_ROOT_PATH, "#{selected_gif.id}.gif")
   end
 end
