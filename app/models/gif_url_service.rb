0c7908effd1cfb23d8eef53587661288fe40da19
Sathyan Mathai <sathyanmathai@Sathyans-MacBook-Pro.local>
2017-05-05 19:14:34 -0400

    Adds better handling of the twilio size limit

--- app/models/gif_url_service.rb
+++ app/models/gif_url_service.rb
@@ -1,8 +1,11 @@
 class GifUrlService
   GIPHY_CDN_ROOT_PATH = "http://i.giphy.com"
+  SIZE_LIMIT = 500000
 
   def self.find_url(query)
-    result = Giphy.search(query, limit: 1).first
+
+    # TODO: Add the ability to go to the next page if none of the images are below the size limit
+    result = Giphy.search(query, limit: 25).find { |gif| gif.original_image.size < SIZE_LIMIT }
     URI.join(GIPHY_CDN_ROOT_PATH, "#{result.id}.gif")
   end
 end
